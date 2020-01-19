ceaa274cba8c2d414b4e7fa9c01a21650499674c
Josh Cavin <jcavin9@gmail.com>
2020-01-19 11:36:25 -0800

    controller spec test for update

--- spec/controllers/vehicles_controller_spec.rb
+++ spec/controllers/vehicles_controller_spec.rb
@@ -44,4 +44,46 @@ RSpec.describe VehiclesController, type: :controller do
       end
     end
   end
+
+  describe 'POST #update_fuel_efficiency' do
+    let(:vehicle) { create(:vehicle) }
+    let(:fuel_entries) do
+      [
+        {
+          'usage_in_mi' => '500',
+          'us_gallons' => '29.34'
+        },
+        {
+          'usage_in_mi' => '250',
+          'us_gallons' => '49.34'
+        }
+      ]
+    end
+
+    context 'when vehicle updated' do
+      before do
+        allow(FleetioRuby::FuelEntry).to receive(:filter) { fuel_entries }
+
+        post :update_fuel_efficiency, format: :json, params: { id: vehicle.id }
+      end
+
+      it 'returns status of 201' do
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'when vehicle not updated' do
+      before do
+        allow(FleetioRuby::FuelEntry).to receive(:filter) do
+          { reason_phrase: 'not found', status: '404' }
+        end
+
+        post :update_fuel_efficiency, format: :json, params: { id: vehicle.id }
+      end
+
+      it 'returns status of 422' do
+        expect(response.status).to eq(422)
+      end
+    end
+  end
 end
